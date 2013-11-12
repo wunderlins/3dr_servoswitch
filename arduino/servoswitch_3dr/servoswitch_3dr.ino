620087c24aedf064d6d95aa0d74d3b1c465382a7
wus <swunderlin@gmail.com>
2013-11-12 20:38:37 +0100

    first working version for 3 pos switch

--- arduino/servoswitch_3dr/servoswitch_3dr.ino
+++ arduino/servoswitch_3dr/servoswitch_3dr.ino
@@ -7,49 +7,102 @@
  * This firmware makes it possible to use a 3 position switch on the transmitter.
  * depending on the position of the r/c channel, pulses are sent over a USB 
  * cable to a canon camera. 
- *    pos 0 (< 1300 ns): no action
- *    pos 1 (> 1300 && < 1700ns): camera shutter, 100ms pulse
- *    pos 2 (> 1700ns): camera off, 1000ms pulse
+ *    pos 0 (< 1333 ns): no action
+ *    pos 1 (> 1333 && < 1666ns): camera shutter, 100ms pulse
+ *    pos 2 (> 1666ns): camera off, 1000ms pulse
  *
- * This program is made to be used with ATtiny45/85
+ * This program is made to be used with ATtiny45/85. It is programmed by a lazy
+ * programmer, sending impulses is blocking. I couldn't be arsed to use 
+ * interrupts.
  *
- * $Id$
+ * (c) 2013, Simon Wunderlin <swunderlin@gmail.com>
  */
 
 // pin setup
-#define PIN_PPM 2
 #define PIN_CAM 1
+#define PIN_PPM 2
 #define PIN_LED 3
 
+#define PULSE_TRIGGER   250
+#define PULSE_SHUTDOWN 1000
+
 unsigned long duration, lastgood = 0;
-int ppm_in = 0;
+uint8_t switch_pos, good_frames, last_switch_pos = 0;
 
 void setup() {
 	pinMode(PIN_CAM, OUTPUT);
 	digitalWrite(PIN_CAM, LOW);
 	delay(5000); // make sure the pin is low
 	
-	pinMode(PIN_PPM, INPUT);
-	pinMode(PIN_CAM, OUTPUT);
+	pinMode(PIN_LED, OUTPUT);
+	digitalWrite(PIN_LED, LOW);
 	
+	pinMode(PIN_PPM, INPUT);
+}
+/**
+ * Toggle switch for N milliseconds
+ *
+ * This method is blocking (uses delay()).
+ */
+void send_pulse(int duration_ms) {
+	digitalWrite(PIN_LED, HIGH);
+	digitalWrite(PIN_CAM, HIGH);
+	delay(duration_ms);
+	digitalWrite(PIN_LED, LOW);
+	digitalWrite(PIN_CAM, LOW);
 }
 
 void loop() {
   
 	/**
 	 * check ppm signal
+	 * 
 	 * the length of the pulse (in microseconds) or 0 if no pulse 
 	 * started before the timeout (unsigned long)
-	 *
-	 * ppm_in == 0 means the camera should be off or turned off
-	 * ppm_in == 1 means the camera should be on or turned on
 	 */
 	duration = pulseIn(PIN_PPM, HIGH, 20000); 
-	if (duration == 0)
+	if (duration == 0) { // no ppm signal
 		duration = lastgood;
-	else
+		good_frames = 0;
+	} else {
 		lastgood = duration;
-	ppm_in = map(lastgood, 1000, 2000, 0, 1);
+		good_frames++;
+	}
+	
+	// check if we subsequently got 10 good frames, otherwise skip actions.
+	// this tries to deal with noisy radios
+	if (good_frames < 15)
+		return;
+	
+	// map ppm input values to switch states (0-2)
+	//  0: no signal
+	//  1: trigger camera
+	//  2: shut down cam
+	if (lastgood > 1666) switch_pos = 2;
+	else if (lastgood > 1333) switch_pos = 1;
+	else switch_pos = 0;
+	
+	// decide what to do with the pulse we have
+	if (switch_pos != last_switch_pos) {
+		switch (switch_pos) {
+			case 0: // no action
+				digitalWrite(PIN_LED, LOW); // turn off the lights
+				digitalWrite(PIN_CAM, LOW); // no signal or off
+				break;
+			
+			case 1: // send trigger pulse
+				send_pulse(PULSE_TRIGGER);
+				break;
+			
+			case 2:
+				send_pulse(PULSE_SHUTDOWN);
+				break;
+		}
+		
+		last_switch_pos = switch_pos;
+	}
+	
+	good_frames = 0;
 	
 	/*
 	// if ppm_in is high and cam_state is low, turn the camera on
